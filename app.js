5018dfa4d46e7969e466c63592de9bcacb8bbe07
Brian Kim <brian.kyounghoon.kim@gmail.com>
2020-11-26 12:30:58 -0500

    ADD:NOV 26, login and register

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express') //import express
 const app = express() // initialize express
 const port = 3000 //setting deport
+const mongoose = require('mongoose');
+const { Schema } = mongoose; //Grab the schema object from mongoose
+require('dotenv').config();
+
+mongoose.connect('mongodb+srv://netflix-api-dev:u45UXljBRHflrr13@cluster0.dksk4.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority', {
+  useCreateIndex: true, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+const User = mongoose.model('Users', new Schema(
+  { 
+    name: String, 
+    email: {
+      type: String,
+      required: true, 
+      unique: true
+    }, 
+    password: {
+      type: String, 
+      required: true
+    }, 
+
+  }));
+
+
+app.use(express.json());
 
 // using the get method
 // Logic for the Get Request
@@ -9,25 +36,46 @@ app.get('/', (req, res) => {
   res.send("Hi")
 });
 
-app.use(express.json());
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    name: req.body.name, 
+    email: req.body.email, 
+    password: req.body.password
+  })
+
+  newUser.save((err, user)=>{
+    if(err){
+      console.log(err);
+      res.send(400, {
+        status: err
+      })
+    } else {
+      console.log("All is good");
+      console.log(user);
+      res.send("resgister");
+    }
+  })
+})
 
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const validUser = {
-    email: 'briankim@gmail.com',
-    password: '1234',
-  }
-  if (email == validUser.email && validUser.password) {
-    res.send({
-      status: 'valid'
+  User.findOne({
+    email: email,
+    password: password
+  }, (err, user) => {
+    if(user) {
+      res.send({
+        status: "Valid"
     });
-  } else {
-    res.send(404, {
-      status: "Not Found"
-    })
-  }
-  console.log(req.body);
+  }else{
+      res.send(404, {
+        status: "Not Found"
+      
+      })
+    }
+  });
+
 })
 
 // starting the application
---
+++ test.md
@@ -0,0 +1,5 @@
+u45UXljBRHflrr13
+
+netflix-api-dev
+
+mongodb+srv://netflix-api-dev:<password>@cluster0.dksk4.mongodb.net/<dbname>?retryWrites=true&w=majority
